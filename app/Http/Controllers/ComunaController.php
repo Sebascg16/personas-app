432f76a3df782dd1b4d703564d95fdca8a93ec4b
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-03-27 17:08:08 -0500

    Ajuste de la vista del listado de comunas

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -15,8 +15,12 @@ class ComunaController extends Controller
      */
     public function index()
     {
-        $comunas = Comuna::all();
-        return view('comuna.index', ['comunas' => $comunas]);
+        //$comunas = Comuna::all();
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+        ->get();
+        return view('comuna.index', ['comunas' => $comunas]);   
     }
 
     /**
