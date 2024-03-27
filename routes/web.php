60a1f906d48dfe0f52237da0b4b51aa7c98533a0
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-03-27 16:39:57 -0500

    https://github.com/Sebascg16/personas-app.git into main

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
+|   
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comunas', [ComunaController::class, 'index']) -> name('comunas.create');
