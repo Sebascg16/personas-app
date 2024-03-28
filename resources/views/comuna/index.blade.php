42b1287ea78f2ee52c377a9fd46da7d8fb6d7ab9
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-03-28 11:50:51 -0500

    Commune Edit

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -32,11 +32,14 @@
       <td>{{ $comuna->comu_nomb }}</td>
       <td>{{ $comuna->muni_nomb}}</td>
       <td>
+        <a href="{{route('comunas.edit', ['comuna'=>$comuna->comu_codi]) }}"
+          class="btn btn-info"> Edit</a></li>
+
         <form action="{{ route('comunas.destroy', ['comuna' => $comuna->comu_codi]) }}"
-        method='POST' style="display: inline-block">
-        @method('delete')
-        @csrf
-        <input class="btn btn-danger" type="submit" value="Delete">
+          method='POST' style="display: inline-block">
+          @method('delete')
+          @csrf
+          <input class="btn btn-danger" type="submit" value="Delete">
         </form>
       </td>
     </tr>
