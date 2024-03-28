4bc3a964977fb59c845eadfc2bef11c880fa3144
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-03-28 11:30:15 -0500

    Commune Delete

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/', function () {
 Route::get('/comunas', [ComunaController::class, 'index']) -> name('comunas.index');
 Route::POST('/comunas', [ComunaController::class, 'store']) -> name('comunas.store');
 Route::get('/comunas/create', [ComunaController::class, 'create']) -> name('comunas.create');
+Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy']) -> name('comunas.destroy');
