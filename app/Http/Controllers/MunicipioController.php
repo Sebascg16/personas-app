98efda6f0e90bb6a95a9e021bc67a050d5c7e071
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-03-28 13:29:14 -0500

    Municipality Edit

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -77,7 +77,11 @@ class MunicipioController extends Controller
      */
     public function edit($id)
     {
-        //
+        $municipio = Municipio::find($id);
+        $departamentos = DB::table('tb_departamento')
+            ->orderBy('depa_nomb')
+            ->get();
+        return view('municipio.edit', ['municipio' => $municipio, 'departamentos' => $departamentos]);
     }
 
     /**
@@ -89,7 +93,18 @@ class MunicipioController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $municipio = Municipio::find($id);
+        
+        $municipio->muni_nomb = $request->name;
+        $municipio->depa_codi = $request->code;
+        $municipio->save();
+
+        $municipios = DB::table('tb_municipio')
+        ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*', "tb_departamento.depa_nomb")
+        ->get();
+
+        return view('municipio.index', ['municipios' => $municipios]);
     }
 
     /**
@@ -107,7 +122,7 @@ class MunicipioController extends Controller
         ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
         ->select('tb_municipio.*', "tb_departamento.depa_nomb")
         ->get();
-        
+
         return view('municipio.index', ['municipios' => $municipios]);
     }
 }
