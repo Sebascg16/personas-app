95a5d4c8b72b2bf82409d968e3900f7483678328
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-03-28 14:12:41 -0500

    Departamento Lista

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ComunaController;
 use App\Http\Controllers\MunicipioController;
+use App\Http\Controllers\DepartamentoController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -34,3 +35,6 @@ Route::get('/municipios/create',[MunicipioController::class, 'create'])-> name('
 Route::delete('/municipios/{municipio}', [MunicipioController::class, 'destroy']) ->name('municipios.destroy');
 Route::put('/municipios/{municipio}', [MunicipioController::class, 'update']) -> name('municipios.update');
 Route::get('/municipios/{municipio}/edit', [MunicipioController::class, 'edit']) -> name('municipios.edit');
+
+//Rutas departamentos
+Route::get('/departamentos', [DepartamentoController::class, 'index']) -> name ('departamento.index');
