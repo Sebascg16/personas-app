60cea9b032c1113056829e36df17984a18236886
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-03-28 12:18:17 -0500

    Municipio List

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -25,3 +26,6 @@ Route::get('/comunas/create', [ComunaController::class, 'create']) -> name('comu
 Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy']) -> name('comunas.destroy');
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update']) -> name('comunas.update');
 Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit']) -> name('comunas.edit');
+
+//Rutas Municipio
+Route::get('/municipios', [MunicipioController::class, 'index']) -> name('municipios.index');
